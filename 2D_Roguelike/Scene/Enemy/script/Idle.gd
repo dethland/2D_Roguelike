66c18f7e2a18dd922ae45c4cf7df6e149d2cc1be
Brandon George <georgb3@rpi.edu>
2022-02-08 16:59:57 -0500

    Small Search fixes

--- 2D_Roguelike/Scene/Enemy/script/Idle.gd
+++ 2D_Roguelike/Scene/Enemy/script/Idle.gd
@@ -6,6 +6,7 @@ var running_timer := 0
 var stop_timer:=0
 var Stop_Or_Run:=true 
 var ram_stop:=0
+
 # enemy willl search in a range ,it will change direcation when touch a wall or move at the edge of range
 #to make it more real, enemy will ramdomly choose stop or run
 # stop_or_run = ture when running ,false when stoping
@@ -14,23 +15,31 @@ func _ready() -> void:
 	#velocity.x = direction.x * speed
 	ram_stop=randi() % 300
 	running_timer=0
-func _physics_process(delta: float)-> void:
-	#if is_able:
-	if Stop_Or_Run :
-		if (player.is_on_wall() or running_timer >= 500) : 
-			# run to a wall or out range in running mode
-			velocity*=-1.0
-			running_timer=0
-		elif running_timer == ram_stop :
-			#when running find stop point ,ramdomlize next stop point ,start stoping count down
-			ram_stop = randi()%500
-			Stop_Or_Run = false
-		else:
-			velocity.y = player.move_and_slide(velocity, Vector2.UP).y
-			running_timer+=1
+	
+
+func _process(delta):
+	if(enemy.get("is_chasing")):
+		is_able = false
 	else:
-		if stop_timer >= 100 :
-			# stop end start running 
-			Stop_Or_Run = true
-			stop_timer=0
-		stop_timer+=1
+		is_able = true
+
+func _physics_process(delta: float)-> void:
+	if is_able:
+		if Stop_Or_Run :
+			if (enemy.is_on_wall() or running_timer >= 500) : 
+				# run to a wall or out range in running mode
+				velocity*=-1.0
+				running_timer=0
+			elif running_timer == ram_stop :
+				#when running find stop point ,ramdomlize next stop point ,start stoping count down
+				ram_stop = randi()%500
+				Stop_Or_Run = false
+			else:
+				velocity.y = enemy.move_and_slide(velocity, Vector2.UP).y
+				running_timer+=1
+		else:
+			if stop_timer >= 100 :
+				# stop end start running 
+				Stop_Or_Run = true
+				stop_timer=0
+			stop_timer+=1
--- 2D_Roguelike/Scene/Enemy/script/Search.gd
+++ 2D_Roguelike/Scene/Enemy/script/Search.gd
@@ -3,8 +3,9 @@ extends "res://Scene/Enemy/script/enemy_script_template.gd"
 var detect : Area2D
 var objs_in_area : Array
 var velocity = Vector2.ZERO
-var speed = 60
+var speed = 70
 var max_distance = 10
+var is_chasing = false
 
 func _ready():
   detect = get_parent().get_parent().get_node("player_detection")
@@ -13,14 +14,16 @@ func _process(delta):
   objs_in_area = detect.get_overlapping_bodies()
 
 func _physics_process(delta):
-  if objs_in_area.size() > 1:
-	  for obj in objs_in_area:
-		  if obj.get("is_player"):
-			  velocity = (obj.position-enemy.position).normalized() * speed
-  else:
-	  velocity = Vector2.ZERO
-  
-  velocity = enemy.move_and_slide(velocity, Vector2.UP)
+	if objs_in_area.size() > 1:
+		for obj in objs_in_area:
+			if obj.get("is_player"):
+				velocity = (obj.position-enemy.position).normalized() * speed
+				is_chasing = true
+	else:
+		velocity = Vector2.ZERO
+		is_chasing = false
+	
+	velocity = enemy.move_and_slide(velocity, Vector2.UP)
 
   
 
