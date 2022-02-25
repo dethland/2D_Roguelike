1ce096941e15875729a88ff64e18e376f6a11a72
Smolae <93052984+Smolae@users.noreply.github.com>
2022-02-25 16:29:38 -0500

    Revert "Merge branch 'main' of https://github.com/dethland/2D_Roguelike"
    This reverts commit a5d921d472d4e8adc6b5137acb8ee7dedf0599a7, reversing
    changes made to acf241d583886818041791d6793e64e77fc56191.

--- 2D_Roguelike/Scene/Enemy/script/Search.gd
+++ 2D_Roguelike/Scene/Enemy/script/Search.gd
@@ -8,28 +8,29 @@ var max_distance = 10
 var is_chasing = false
 
 func _ready():
-  detect = get_parent().get_parent().get_node("Search_range")
+#  detect = get_parent().get_parent().get_node("player_detection")
+	pass
 
 func _process(delta):
-  objs_in_area = detect.get_overlapping_bodies()
-	
+#  objs_in_area = detect.get_overlapping_bodies()
+	pass
 
 func _physics_process(delta):
-	if is_able && objs_in_area.size() > 1:
-		for obj in objs_in_area:
-			if obj.get("is_player"):
-				velocity = (obj.position-enemy.position).normalized() * speed
-				is_chasing = true
-				is_able = false
-			else:
-				velocity = Vector2.ZERO
-				is_chasing = false
-				is_able = true
-	else:
-		velocity = Vector2.ZERO
-		is_chasing = false
-		is_able = true
-	
-	velocity.y = 0
-	velocity = enemy.move_and_slide(velocity, Vector2.UP)
-	
+#	if objs_in_area.size() > 1 && is_able:
+#		for obj in objs_in_area:
+#			if obj.get("is_player"):
+#				velocity = (obj.position-enemy.position).normalized() * speed
+#				is_chasing = true
+#				is_able = false
+#			else:
+#				velocity = Vector2.ZERO
+#				is_chasing = false
+#				is_able = true
+#	else:
+#		velocity = Vector2.ZERO
+#		is_chasing = false
+#		is_able = true
+#
+#	velocity = enemy.move_and_slide(velocity, Vector2.UP)
+	if is_able:
+		pass
