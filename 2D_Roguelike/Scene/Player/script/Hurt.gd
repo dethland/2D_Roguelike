c13fa1366246af22be69d792b53e67356b2b5136
Kevin-771 <70790256+Kevin-771@users.noreply.github.com>
2022-03-04 17:18:03 -0500

    made hurt handle parry

--- 2D_Roguelike/Scene/Player/script/Hurt.gd
+++ 2D_Roguelike/Scene/Player/script/Hurt.gd
@@ -1,9 +1,47 @@
-extends "res://Scene/Player/script/Parry.gd"
+extends "res://Scene/Player/script/player_script_template.gd"
+
+var is_parry = false
+
+const parry_lockout = 5
+const parry_duration = 3
+var curr_duration = parry_duration
+var curr_lockout = parry_lockout
 
 func turn_on()->void:
 	.turn_on()
 	animator._play("hurt");
 
+func _ready():
+	is_parry = false
+
+func test_parry() -> bool:
+	return is_parry
+	
+func sucessful_parry() -> void:
+	is_parry = false
+	statemachine.change_state_to("Idle")
+	print("parried")
+
+func unsucessful_parry() -> void:
+	is_parry = false
+	statemachine.change_state_to("Idle")
+	print("failed")
+
+func _physics_process(delta):
+	#print(parry_lockout)
+	if is_parry:
+		curr_duration -= delta
+		if curr_duration <= 0:
+			statemachine.change_state_to("Idle")
+			is_parry = false
+			curr_duration = parry_duration
+	elif curr_lockout > 0:
+		curr_lockout -= delta
+	elif Input.is_action_just_pressed("parry"):
+		is_parry = true
+		statemachine.change_state_to("Parry")
+		# play parry animation
+		print("parry")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "hurt":
@@ -11,6 +49,7 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 
 
 func hurt():
+	print("hurting")
 	var parry = test_parry()
 	if parry:
 		sucessful_parry()
--- 2D_Roguelike/Scene/Player/script/Parry.gd
+++ 2D_Roguelike/Scene/Player/script/Parry.gd
@@ -1,38 +1,3 @@
 extends "res://Scene/Player/script/player_script_template.gd"
 
-var is_parry = false
-
-var parry_lockout = 1
-var parry_duration = 1
-
-func _ready():
-	is_parry = false
-
-func test_parry() -> bool:
-	return is_parry
-	
-func sucessful_parry() -> void:
-	is_parry = false
-	statemachine.change_state_to("Idle")
-	print("parried")
-
-func unsucessful_parry() -> void:
-	is_parry = false
-	statemachine.change_state_to("Idle")
-	print("failed")
-
-func _physics_process(delta):
-	#print(parry_lockout)
-	if is_parry:
-		parry_duration -= delta
-		if parry_duration <= 0:
-			statemachine.change_state_to("Idle")
-			is_parry = false
-
-	if parry_lockout > 0:
-		parry_lockout -= delta
-	elif Input.is_action_just_pressed("parry"):
-		is_parry = true
-		statemachine.change_state_to("Parry")
-		print("parry")
 
