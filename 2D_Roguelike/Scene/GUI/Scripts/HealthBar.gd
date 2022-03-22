22d486622a6ec4a5e8e7e4174ab8446d9c130043
Brandon George <georgb3@rpi.edu>
2022-03-22 19:44:54 -0400

    Health bar interpolation is functional
    Health bar value interpolates from prev health to new health when damage is taken

--- 2D_Roguelike/Scene/GUI/Scripts/HealthBar.gd
+++ 2D_Roguelike/Scene/GUI/Scripts/HealthBar.gd
@@ -1,18 +1,27 @@
 extends Control
 
 
-var bar : TextureProgress
+onready var bar = $Container/Bars/Health/HealthBar
+onready var tween = $Container/Bars/Health/Tween
 var player : KinematicBody2D
 var prev_health : int
+var curr_health : int
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	bar = get_node("HBoxContainer").get_node("Bars").get_node("Health").get_node("HealthBar")
-	player = get_parent().get_parent().get_node("Player")
+	#player = get_parent().get_parent().get_node("Player")
+	player = $"../../Player"
+	bar.value = player.health
+	prev_health = player.health
 
 func _process(delta):
-	bar.value = player.health
+	curr_health = player.health
+	if(curr_health != prev_health):
+		tween.interpolate_property(bar, "value", prev_health, curr_health, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween.start()
+	
+	prev_health = curr_health
 	# Check if current health is different from last recorded health
 	# Wait half a second before changing second bar
 	
