2ae063c1ba7df0a17d2ab2fa9214a03f36c729de
Brandon George <bdngeorge7@gmail.com>
2022-04-11 14:07:39 -0400

    Small changes

--- 2D_Roguelike/Scene/GUI/Scripts/camera.gd
+++ 2D_Roguelike/Scene/GUI/Scripts/camera.gd
@@ -1,7 +1,10 @@
 extends Camera2D
 
 onready var player = get_parent().get_node("Player")
+
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+
 #This function gets called every frame
 func _process (delta):
-  
   position = player.position
